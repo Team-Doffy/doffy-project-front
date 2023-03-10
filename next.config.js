ae39a43b07b07e13b4d9fbbe7a4983ce4cc0b0ef
KIMTAEKJU <gkffntm12@naver.com>
2023-03-10 12:17:39 +0900

    feat: next 설정 추가
    - SWC 최적화 옵션 추가
    - 배포 시 콘솔 제거 옵션 추가

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // compiler: {
-  //   // styledComponents: true, // styled-components ssr 활성화
-  //   // removeConsole: process.env.NODE_ENV === "production", // 배포 시 콘솔 제거
-  // },
+  compiler: {
+    styledComponents: true, // styled-components ssr 활성화
+    removeConsole: process.env.NODE_ENV === "production", // 배포 시 콘솔 제거
+  },
+  swcMinify: true, // SWC 최적화
   
 }
 
