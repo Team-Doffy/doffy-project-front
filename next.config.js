4e72ed627b460ce3b57150c5e798eb6f9041d4a4
KIMTAEKJU <gkffntm12@naver.com>
2023-03-10 13:42:36 +0900

    chore: 개발 실행 옵션 변경
    - 개발 실행 시 compiler, swcMinify 옵션을 터보팩에서 사용하지 못하여 배포에서만 사용하도록 수정

--- next.config.js
+++ next.config.js
@@ -3,12 +3,18 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  compiler: {
+}
+
+/**
+ * 터보팩에서 아래 옵션 추가 시 동작하지 않아
+ * 배포일때만 옵션을 추가하도록 수정
+ */
+if (process.env.MODE === 'production') {
+  nextConfig.compiler = {
     styledComponents: true, // styled-components ssr 활성화
-    removeConsole: process.env.NODE_ENV === "production", // 배포 시 콘솔 제거
-  },
-  swcMinify: true, // SWC 최적화
-  
+    removeConsole: process.env.NODE_ENV === 'production', // 배포 시 콘솔 제거
+  };
+  nextConfig.swcMinify = true
 }
 
 export default nextConfig;
